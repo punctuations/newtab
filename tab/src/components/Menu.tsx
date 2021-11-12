9f7d4abb8ea810a7e3c02211eca4fb0548f08a5e
Matt <matt@dont-ping.me>
2021-11-11 20:16:53 -0800

    fix: add transform

--- tab/src/components/Menu.tsx
+++ tab/src/components/Menu.tsx
@@ -69,7 +69,7 @@ export function Menu() {
             animate={{ opacity: 1, scale: 1 }}
             exit={{ opacity: 0, scale: 0.5 }}
             transition={{ type: "spring", duration: 0.5 }}
-            className="z-40 bg-white dark:bg-black absolute -translate-y-1/2 -translate-x-1/2 top-1/2 left-1/2 flex flex-col items-start space-y-4 shadow-2xl dark:shadow-none dark:border border-gray-700 rounded-md px-5 py-6"
+            className="z-40 bg-white dark:bg-black absolute transform -translate-y-1/2 -translate-x-1/2 top-1/2 left-1/2 flex flex-col items-start space-y-4 shadow-2xl dark:shadow-none dark:border border-gray-700 rounded-md px-5 py-6"
           >
             <div className="flex w-full justify-between">
               <p className="p-1 rounded bg-gray-200 dark:bg-gray-800 text-gray-500 text-xs font-bold">
